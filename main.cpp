ba5862cec1fc43ef88d1c97ae8404cef2a9f01e0
Bogan Tverdoi <bogdan.tverdoy@gmail.com>
2023-03-27 19:40:22 +0400

    81. Search in Rotated Sorted Array II. Medium

--- main.cpp
+++ main.cpp
@@ -11,80 +11,90 @@
 using namespace std;
 
 /*
-33. Search in Rotated Sorted Array
-There is an integer array nums sorted in ascending order (with distinct values).
+81. Search in Rotated Sorted Array II
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
 
-Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) 
-such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
-For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array 
+is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated 
+at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
 
-Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
 
-You must write an algorithm with O(log n) runtime complexity.
-
+You must decrease the overall operation steps as much as possible.
 1 <= nums.length <= 5000
 -104 <= nums[i] <= 104
-All values of nums are unique.
-nums is an ascending array that is possibly rotated.
+nums is guaranteed to be rotated at some pivot.
 -104 <= target <= 104
 */
 
-	int binarySearch(vector<int>& nums, int target, int l, int r){
+	bool binarySearch(vector<int>& nums, int target, int l, int r){
 		int ind = l + (r - l)/2;
 		while ( l >=0 && r < nums.size() && l <= r){
 			ind = l + (r - l)/2;
 			if (nums[ind] == target){
-				return ind;
+				return true;
 			}
 			if (nums[ind] > target){
-				r = ind -1;
-			}
-			else {
-				l = ind +1;
-			}
+					r = ind - 1;
+					while ( (r >= l) && (nums[r] == nums[ind]) )
+						--r;
+				}
+				else{
+					l = ind +1;
+					while ( (l <= r) && (nums[l] == nums[ind]) )
+						++l;
+				}
 		}
-		return -1;
+		return false;
 	}
 
-    int search(vector<int>& nums, int target) {
+    bool search(vector<int>& nums, int target) {
 		if ((nums.size() == 1) && nums[0] == target)
-			return 0;
-		int l = 0, r = nums.size() - 1, ind = l;
+			return true;
 		
+		int l = 0, r = nums.size() - 1, ind = l;
 		while (l <= r){
-			ind = l+ (r - l)/2;
-			
-			//not rotated
-			if (nums[l] < nums[r])
-				return binarySearch(nums, target, l, r);
-			
+			ind = l+ (r - l)/2;			
 			//rotated
 			if (nums[ind] == target)
-				return ind;
+				return true;
 			
-			
-			if (nums[ind] >= nums[l]){//we are in the higher side of the array
-				if ( (nums[ind] > target) && (target >= nums[l])) {//target is between l and ind;
+			if (nums[l] == nums[r]){
+				if (nums[l] == target)
+					return true;
+				++l; --r;
+				continue;
+			}
+			if (nums[ind] >= nums[l]){
+				if ( (nums[ind] > target) && (target >= nums[l]) ) {
 					return binarySearch(nums, target, l, ind - 1);
 				}
-				else
-					l = ind +1;
+				else{
+						l = ind +1;
+						while ( (l <= r) && (nums[l] == nums[ind]) )
+							++l;
+				}
 			}
 			else if ( nums[ind] <= nums[r]){// we are in the lower side of the array
 				if ( (nums[ind] < target) && (target <= nums[r])) {// target is between ind and r
 					return binarySearch(nums, target, ind + 1, r);
 				}
-				else
+				else{
 					r = ind - 1;
+					while ( (r >= l) && (nums[r] == nums[ind]) )
+						--r;
+				}
 			}
 		}
-		return -1;
+		return false;
     }
 
 int main(){
-	vector<int> nums = {4,5,6,7,0,1,2};
-	int target = 0;
-	std::cout << search(nums, target) << "\n";
+	vector<int> nums = {3,4,4,4,4,4,4,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,8,8,9,9,9,9,9,9,9,9,9,10,10,10,-10,-10,-10,-9,-8,
+	-8,-8,-8,-8,-7,-7,-7,-7,-6,-6,-6,-6,-6,-6,-6,-5,-5,-5,-4,-4,-4,-4,-3,-3,-3,-3,-3,-3,-2,-2,-2,-2,-1,-1,0,0,0,1,1,1,1,1,1,2,2,
+	2,2,2,2,2,2,3,3,3};
+	int target = 2;
+	std::cout << boolalpha << search(nums, target) << "\n";
 	
 
 }
