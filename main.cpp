a6f4da065ea1cbf6066127b8bed838135445ec70
Bogan Tverdoi <bogdan.tverdoy@gmail.com>
2023-03-28 14:14:34 +0400

    17. Letter Combinations of a Phone Number.Medium

--- main.cpp
+++ main.cpp
@@ -11,44 +11,57 @@
 using namespace std;
 
 /*
-454. 4Sum II
-Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+17. Letter Combinations of a Phone Number
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
-0 <= i, j, k, l < n
-nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
-n == nums1.length
-n == nums2.length
-n == nums3.length
-n == nums4.length
-1 <= n <= 200
--228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
 */
 
-//implementing solution by abivilion and shwetanknaveen
-int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-	int result = 0;
-	std::map<int,int> oneTwoSum;
+	std::map<int, std::vector<string>> chars{
+		{2, {"a", "b", "c"}},
+		{3, {"d", "e", "f"}},
+		{4, {"g", "h", "i"}},
+		{5, {"j", "k", "l"}},
+		{6, {"m", "n", "o"}},
+		{7, {"p", "q", "r", "s"}},
+		{8, {"t", "u", "v"}},
+		{9, {"w", "x", "y", "z"}}
+		};
 
-	//calculate all possible sums of nums1 and nums2 and record these sums to map
-	for (auto it1:nums1){
-		for (auto it2:nums2){
-			++oneTwoSum[it1 + it2];
+	//  {'a', 'b', 'c'};
+    vector<string> letterCombinations(string digits) {
+		vector<string> result;
+		if (digits.length() == 0)
+			return result;
+		//taking first number and making combination with the rest of numbers
+		//taking the rest of digits and recursively perform combinations search
+		int d = digits[0] - '0';
+		vector<string> temp;
+		string newDig = digits.substr(1,digits.length() - 1); 
+		if(digits.length() > 1){
+			temp = letterCombinations(newDig);
 		}
-	}
-	//calculate all possible combinations of nums3 and nums4
-	//if combination already exists in the map with opposite sign, than increment result
-	for (auto it3:nums3){
-		for (auto it4:nums4){
-			result += oneTwoSum[-(it3 + it4)];
+		if (digits.length() ==1 ){
+			for (auto i:chars[d])
+				result.emplace_back(i);
 		}
-	}
-return result;
-}
+		else{
+			for (auto i:chars[d]){
+				for (auto j:temp){
+					
+					result.emplace_back(i+j);
+				}
+			}
+		}
+		return result;
+    }
 
 int main(){
-	vector<int> nums1 = {-1,-1}, 
-	nums2 = {-1,1}, 
-	nums3 = {-1,1}, 
-	nums4 = {1,-1};
-	std::cout << fourSumCount(nums1, nums2, nums3, nums4) <<"\n";
+	string digits = "234";
+	vector<string> result =  letterCombinations(digits);
+	for (auto i:result)
+	std::cout << i << "\n";
 }
