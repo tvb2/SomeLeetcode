881efe0d17bcb3781a049a132d651e91e93349be
Bogan Tverdoi <bogdan.tverdoy@gmail.com>
2023-03-28 18:56:10 +0400

    6. Zigzag Conversion.Medium. Solution submitted

--- main.cpp
+++ main.cpp
@@ -11,57 +11,58 @@
 using namespace std;
 
 /*
-17. Letter Combinations of a Phone Number
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+6. Zigzag Conversion
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
 
-A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+Write the code that will take a string and make this conversion given a number of rows:
 
-0 <= digits.length <= 4
-digits[i] is a digit in the range ['2', '9'].
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
 */
 
-	std::map<int, std::vector<string>> chars{
-		{2, {"a", "b", "c"}},
-		{3, {"d", "e", "f"}},
-		{4, {"g", "h", "i"}},
-		{5, {"j", "k", "l"}},
-		{6, {"m", "n", "o"}},
-		{7, {"p", "q", "r", "s"}},
-		{8, {"t", "u", "v"}},
-		{9, {"w", "x", "y", "z"}}
-		};
-
-	//  {'a', 'b', 'c'};
-    vector<string> letterCombinations(string digits) {
-		vector<string> result;
-		if (digits.length() == 0)
-			return result;
-		//taking first number and making combination with the rest of numbers
-		//taking the rest of digits and recursively perform combinations search
-		int d = digits[0] - '0';
-		vector<string> temp;
-		string newDig = digits.substr(1,digits.length() - 1); 
-		if(digits.length() > 1){
-			temp = letterCombinations(newDig);
-		}
-		if (digits.length() ==1 ){
-			for (auto i:chars[d])
-				result.emplace_back(i);
-		}
-		else{
-			for (auto i:chars[d]){
-				for (auto j:temp){
-					
-					result.emplace_back(i+j);
+    string convert(string s, int numRows) {
+		if (numRows == 1)
+			return s;
+		string result;
+		int len = s.length();
+		size_t row = 0;
+		while (row < numRows){
+			int pos = row;
+			if (row == 0 || row == numRows - 1){
+				while(pos < len){
+					result += s[pos];
+					pos = pos + (2*numRows - 2);
 				}
+				++row;
+			}
+			else{
+				bool toggle = true;
+				int temp = pos;
+				while (temp < len){
+					result += s[temp];
+					if (toggle){
+						temp = pos + (2*numRows - 2 - 2*row);
+						toggle = false;
+					}
+					else{
+						temp = pos + (2*numRows - 2);
+						pos = temp;
+						toggle = true;
+					}
+				}
+				++row;
 			}
 		}
 		return result;
     }
 
 int main(){
-	string digits = "234";
-	vector<string> result =  letterCombinations(digits);
-	for (auto i:result)
-	std::cout << i << "\n";
+	string s = "b";//3: PAHNAPLSIIGYIR 4: PINALSIGYAHRPI
+	int numOfRows = 1;
+	std::cout << convert(s, numOfRows) << "\n";
 }
