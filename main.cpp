50c0fc83b831d3eb53f70f3a3d1c58d0df050bff
Bogan Tverdoi <bogdan.tverdoy@gmail.com>
2023-03-30 07:31:45 +0400

    32. Longest Valid Parentheses. Hard

--- main.cpp
+++ main.cpp
@@ -8,61 +8,83 @@
 #include <bits/stdc++.h>
 #include <iterator>
 #include <functional>
+#include <stack>
 using namespace std;
 
 /*
-6. Zigzag Conversion
-The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-P   A   H   N
-A P L S I I G
-Y   I   R
-And then read line by line: "PAHNAPLSIIGYIR"
+32. Longest Valid Parentheses
+Given a string containing just the characters '(' and ')', return the length of the longest valid (well-formed) parentheses 
+substring.
 
-Write the code that will take a string and make this conversion given a number of rows:
-
-1 <= s.length <= 1000
-s consists of English letters (lower-case and upper-case), ',' and '.'.
-1 <= numRows <= 1000
+0 <= s.length <= 3 * 104
+s[i] is '(', or ')'.
 */
 
-    string convert(string s, int numRows) {
-		if (numRows == 1)
-			return s;
-		string result;
-		int len = s.length();
-		size_t row = 0;
-		while (row < numRows){
-			int pos = row;
-			if (row == 0 || row == numRows - 1){
-				while(pos < len){
-					result += s[pos];
-					pos = pos + (2*numRows - 2);
-				}
-				++row;
+    int longestValidParentheses(string s) {
+		//base case of zero string length
+		if (s.length() == 0)
+			return 0;
+		std::stack<int> p;
+		int i = 0;
+		int max = 0, incr = 2;
+		for (size_t i = 0; i < s.length(); ++i){
+			if (s[i] == '('){
+				p.push(0);
 			}
-			else{
-				bool toggle = true;
-				int temp = pos;
-				while (temp < len){
-					result += s[temp];
-					if (toggle){
-						temp = pos + (2*numRows - 2 - 2*row);
-						toggle = false;
-					}
-					else{
-						temp = pos + (2*numRows - 2);
-						pos = temp;
-						toggle = true;
+			else{// s[i] = ')'
+				if (p.empty())
+					continue;
+				int temp = p.top() + incr;
+				p.pop();
+				/*special case if there was only one element left
+				p = 0 
+				p = 2 (illegal, cannot have closing bracket with single non-zero element left : "())" )
+				*/
+				if (p.empty()){
+					if (temp == incr)
+						p.push(temp);
+					max = (incr > max)?incr:max;
+					continue;
+				}
+				/*
+				initial setup
+				1. p = 2 0 0 -> p = 2 0 temp=2
+				2. p = 2 0 2 -> p = 2 0 temp=4
+				3. p = 2 0   -> p = 2   temp=2
+				4. p = 0 0 2 -> p = 0 0 temp=4
+				5. p = 0 4   -> p = 0   temp=6
+				*/
+				if (temp == incr){//cases 1, 3
+					if (p.top() != 0){//case 3
+						temp += p.top();
+						p.pop();
 					}
 				}
-				++row;
+				else{//cases 2, 4, 5
+					p.pop();
+					/*
+					2. p = 2 0 -> p = 2   temp=4
+					4. p = 0 0 -> p = 0 0 temp=4
+					5. p = 0   -> p = ''  temp=6
+					*/
+					if (!p.empty() && p.top() != 0){//case 2, 5
+						temp += p.top();
+						p.pop();
+					}
+				}
+				p.push(temp);
+				max = (temp > max)?temp:max;
 			}
 		}
-		return result;
+		return max;
     }
 
 int main(){
-	string s = "b";//3: PAHNAPLSIIGYIR 4: PINALSIGYAHRPI
-	int numOfRows = 1;
-	std::cout << convert(s, numOfRows) << "\n";
+	// string s = ")()())";
+	// string s ="()(())";
+	// string s ="())";
+	// string s =")()())";
+	// string s ="(()())";
+	string s =")()())()()(";
+	std::cout << longestValidParentheses(s) << "\n";
 }
