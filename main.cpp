15f4a9eb8bf584a7dab8af2c71e64b01b1253e58
Bogdan Tvedoi <bogdan.tverdoy@gmail.com>
2023-03-30 13:14:54 +0400

    94. Binary Tree Inorder Traversal. Easy

--- main.cpp
+++ main.cpp
@@ -13,96 +13,66 @@
 using namespace std;
 
 /*
-232. Implement Queue using Stacks
-Implement a first in first out (FIFO) queue using only two stacks. 
-The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+94. Binary Tree Inorder Traversal
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
-Implement the MyQueue class:
-
-void push(int x) Pushes element x to the back of the queue.
-int pop() Removes the element from the front of the queue and returns it.
-int peek() Returns the element at the front of the queue.
-boolean empty() Returns true if the queue is empty, false otherwise.
-Notes:
-
-You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
-Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as 
-long as you use only a stack's standard operations.
-
-1 <= x <= 9
-At most 100 calls will be made to push, pop, peek, and empty.
-All the calls to pop and peek are valid.
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
 */
+// Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+    
+    void helper (TreeNode *root, std::vector<int> &result){
+        if (root != nullptr){
+            helper(root->left, result);
+            result.push_back(root->val);
+            helper(root->right, result);
+        }
+    }
 
-class MyQueue {
-public:
-    std::stack<int> s1;
-    std::stack<int> s2;
-    MyQueue() {
+    //recursive version using helper
+    vector<int> inorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        helper(root, result);
+        return result;
     }
-    
-    void push(int x) {
-        s1.push(x);
-    }
-    
-    int pop() {
-        int t = 0, pop = 0;
-        for (size_t i = s1.size(); i > 0; --i){
-            if (s1.size() == 1){
-                pop = s1.top();
-                s1.pop();
-                break;
-            }
-            s2.push(s1.top());
-            s1.pop();
-        }
-        for (size_t i = s2.size(); i > 0; --i){
-            s1.push(s2.top());
-            s2.pop();
-        }
-    return pop;
-    }
-    
-    int peek() {
-        int t = 0, peek = 0;
-        for (size_t i = s1.size(); i > 0; --i){
-            if (s1.size() == 1){
-                peek = s1.top();
-                break;
-            }
-            s2.push(s1.top());
-            s1.pop();
-        }
-        for (size_t i = s2.size(); i > 0; --i){
-            s1.push(s2.top());
-            s2.pop();
-        }
-    return peek;
-    }
-    
-    bool empty() {
-        return s1.empty();
-    }
-};
+    //stack version
+    // vector<int> inorderTraversal(TreeNode* root) {
+    //     std::vector<int> res;
+    //     std::stack<TreeNode*> stack;
+    //     TreeNode *curr = root;
+    //     while (curr != nullptr || !stack.empty()) {
+    //         while (curr != nullptr) {
+    //             stack.push(curr);
+    //             curr = curr->left;
+    //         }
+    //         curr = stack.top();
+    //         stack.pop();
+    //         res.push_back(curr->val);
+    //         curr = curr->right;
+    //     }
+    //     return res;
+    // }
 
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
 
 int main(){
-	std::vector<int> vec {4, 5, 6, 7};
-    MyQueue* obj = new MyQueue();
-    for (auto x:vec)
-        obj->push(x);
-    int param_2 = obj->pop();
-    std::cout << "p2: " << param_2 << "\n";
-    int param_3 = obj->peek();
-    std::cout << "p3: " << param_3 << "\n";
-    bool param_4 = obj->empty();
+
+    // {1,null,2,3};
+    TreeNode *root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    std::vector<int> result = inorderTraversal(root);
+    
+    for (auto i:result)
+        std::cout << i << "\n";
+
  
 }
