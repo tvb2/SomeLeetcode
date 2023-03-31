49312a51539e267e8f8eabecc2e66e13de368948
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-03-31 07:13:08 +0400

    145. Binary Tree Postorder Traversal. Easy

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 using namespace std;
 
 /*
-144. Binary Tree Preorder Traversal
-Given the root of a binary tree, return the preorder traversal of its nodes' values.
+145. Binary Tree Postorder Traversal
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
 
 The number of nodes in the tree is in the range [0, 100].
 -100 <= Node.val <= 100
@@ -31,46 +31,54 @@ The number of nodes in the tree is in the range [0, 100].
     
     void helper (TreeNode *root, std::vector<int> &result){
         if (root != nullptr){
-            result.push_back(root->val);
+            if(root==nullptr)
+                return;
             helper(root->left, result);
-            helper(root->right, result);
+            helper(root->right,result);
+            result.push_back(root->val);
         }
     }
 
     //recursive version using helper
-    // vector<int> preorderTraversal(TreeNode* root) {
-    //     std::vector<int> result;
-    //     helper(root, result);
-    //     return result;
-    // }
-    //stack version
-    vector<int> preorderTraversal(TreeNode* root) {
-        std::vector<int> res;
-        std::stack<TreeNode*> st;
-        TreeNode *curr = root;
-        while (curr != nullptr || !st.empty()) {
-            while (curr != nullptr) {
-                res.push_back(curr->val);
-                st.push(curr);
-                curr = curr->left;
-            }
-            curr = st.top();
-            st.pop();
-            curr = curr->right;
-        }
-        return res;
+    vector<int> postorderTraversal(TreeNode* root) {
+        std::vector<int> result;
+        helper(root, result);
+        return result;
     }
+    //stack version
+    //    vector<int> postorderTraversal(TreeNode* root) {    
+    //    std::vector<int> result;
+    //    std::stack<TreeNode*> st;
+    //    TreeNode *current = root;
+    //    while (current != nullptr || !st.empty()){
+    //         if(current != nullptr){
+    //             st.push(current);
+    //             result.emplace(result.begin(), current->val);
+    //             current = current->right;
+    //         }
+    //         else{
+    //             TreeNode *temp = st.top();
+    //             current = temp->left;
+    //             st.pop();
+    //         }
+    //    }
+    //    return result;
+    // }
 
 
 int main(){
 
     // {1,null,2,3};
-    TreeNode *root = new TreeNode(2);
-    root->right = new TreeNode(3);
-    root->left = new TreeNode(1);
-    root->left->right = new TreeNode(4);
+    TreeNode *root = new TreeNode(25);
+    root->left = new TreeNode(15);
+    root->left->left = new TreeNode(10);
+    root->left->right = new TreeNode(22);
+    
+    root->right = new TreeNode(50);
+    root->right->left = new TreeNode(35);
+    root->right->right = new TreeNode(70);
 
-    std::vector<int> result = preorderTraversal(root);
+    std::vector<int> result = postorderTraversal(root);
     
     for (auto i:result)
         std::cout << i << "\n";
