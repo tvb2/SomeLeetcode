02937b30e7c02ff3786fa839d0d5da33dc8414f6
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-01 08:12:37 +0400

    2601. Prime Subtraction Operation. Medium

--- main.cpp
+++ main.cpp
@@ -13,94 +13,67 @@
 using namespace std;
 
 /*
-224. Basic Calculator
+2601. Prime Subtraction Operation
+You are given a 0-indexed integer array nums of length n.
 
-Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
-Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+You can perform the following operation as many times as you want:
 
-Example 1:
-Input: s = "1 + 1"
-Output: 2
+Pick an index i that you haven’t picked before, and pick a prime p strictly less than nums[i], then subtract p from nums[i].
+Return true if you can make nums a strictly increasing array using the above operation and false otherwise.
 
-Example 2:
-Input: s = " 2-1 + 2 "
-Output: 3
-
-Example 3:
-Input: s = "(1+(4+5+2)-3)+(6+8)"
-Output: 23
-
-
-Constraints:
-1 <= s.length <= 3 * 105
-s consists of digits, '+', '-', '(', ')', and ' '.
-s represents a valid expression.
-'+' is not used as a unary operation (i.e., "+1" and "+(2 + 3)" is invalid).
-'-' could be used as a unary operation (i.e., "-1" and "-(2 + 3)" is valid).
-There will be no two consecutive operators in the input.
-Every number and running calculation will fit in a signed 32-bit integer.
+A strictly increasing array is an array whose each element is strictly greater than its preceding element.
 */
-    //repeating code moved to separate function
-    void process(std::stack<int> &vals, std::stack<char> &ops, int t, int &result){
-    if (!vals.empty() && !ops.empty()){//if already some numbers and operations pending
-                    result = (ops.top() == '+')?(vals.top() + t):(vals.top() - t);
-                    vals.pop(); ops.pop();
-                    vals.push(result);
-                }
-                else if (vals.empty() && !ops.empty()){//if there are no vals but already ops (only ops possible is '-')
-                    vals.push(0-t);
-                    }
-                else{
-                    result = t;
-                    vals.push(t);
-                }
-    }
 
-    int helper (string s, int &i){
-        int result = 0;
-        std::stack<char> ops;//operations
-        std::stack<int> vals;//values
-        for (; i < s.length(); ++i){
-            //a number
-            if(s[i] >= 48 && s[i] <= 57){
-                size_t k = i;
-                std::string v = "";
-                while (s[i] >= 48 && s[i] <= 57){
-                    v += s[i];
-                    ++i;
-                }
-                int t = stoi(v);
-                process(vals, ops, t, result);
-            }
-            if (s[i] == '+' || s[i] == '-')
-                ops.push(s[i]);
-            if (s[i] == '('){
-                int t = helper(s,++i);
-                process(vals, ops, t, result);
-                continue;
-            }
-            if (s[i] == ')'){
-                break;
-            }
+bool isPrime(int n)
+{
+      // Since 0 and 1 is not
+      // prime return false.
+      if(n == 1 || n == 0) return false;
+   
+      // Run a loop from 2 to n-1
+      for(int i = 2; i*i <= n; i++)
+      {
+        // if the number is divisible by i,
+        // then n is not a prime number.
+        if(n % i == 0) return false;
+      }
+      // Otherwise n is a prime number.
+      return true;
+}
+    //return prime numbers below input value
+    std::vector<int> primeNums(int i){
+        std::vector<int> primes;
+        for (int j = 1; j < i; ++j){
+            if (isPrime(j))
+                primes.push_back(j);
         }
-        return vals.top();
+        return primes;
     }
 
-    int calculate(string s) {
-        int i = 0, result = 0;
-        result = helper(s, i);
-        return result;
+    bool primeSubOperation(vector<int>& nums) {
+        std::vector<int> primes;
+        for (size_t i = nums.size() - 1; i > 0; --i ){
+            
+            if(nums[i] <= nums[i - 1]){
+              primes = primeNums(nums[i - 1]);  
+              int k = nums[i - 1];
+              int j = 0;
+                while ((k >= nums[i]) && (j < primes.size()) && (k > 0)){
+                    k = nums[i - 1];
+                    k -= primes[j];
+                    ++j;
+                }
+                nums[i - 1] = k;
+                if (nums[i] <= nums[i - 1])
+                    return false;
+            }           
+        }
+        return true;
     }
 
 
 int main(){
-    // std::string s = "3+587 -100  * 3 / 2 + 4";
-    // std::string s = " 3+5 / 2 "; 
-    // std::string s = " 3/2 ";
-    //  std::string s = "3+2*2";
-    // std::string s = "(1+(4+5+2)-3)+(6+8)";
-    // std::string s = "-1";
-    std::string s = "-(1+2)";
+    std::vector<int> nums = {17,2};
     
-    std::cout << calculate(s) << "\n"; 
+    std::cout << boolalpha << primeSubOperation(nums) << "\n"; 
 }
