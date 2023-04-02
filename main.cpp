3e8c06a8989dd9825a97c005c8081168ec81eaa2
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-02 20:52:43 +0400

    387. First Unique Character in a String. Easy

--- main.cpp
+++ main.cpp
@@ -13,73 +13,41 @@
 using namespace std;
 
 /*
-42. Trapping Rain Water
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+387. First Unique Character in a String
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+1 <= s.length <= 105
+s consists of only lowercase English letters.
 */
-    //editorial brute force
-    //     int trap(vector<int>& height) {
-    //     int ans = 0;
-    //     int size = height.size();
-    //     for (int i = 1; i < size - 1; i++) {
-    //         int left_max = 0, right_max = 0;
-    //         for (int j = i; j >= 0; j--) { //Search the left part for max bar size
-    //             left_max = max(left_max, height[j]);
-    //         }
-    //         for (int j = i; j < size; j++) { //Search the right part for max bar size
-    //             right_max = max(right_max, height[j]);
-    //         }
-    //         ans += min(left_max, right_max) - height[i];
-    //     }
-    //     return ans;
-    // }
 
-    //editorial dynamic programming
-    // int trap(vector<int>& height)
-    // {
-    //     if(height.empty())
-    //         return 0;
-    //     int ans = 0;
-    //     int size = height.size();
-    //     vector<int> left_max(size), right_max(size);
-    //     left_max[0] = height[0];
-    //     for (int i = 1; i < size; i++) {
-    //         left_max[i] = max(height[i], left_max[i - 1]);
-    //     }
-    //     right_max[size - 1] = height[size - 1];
-    //     for (int i = size - 2; i >= 0; i--) {
-    //         right_max[i] = max(height[i], right_max[i + 1]);
-    //     }
-    //     for (int i = 1; i < size - 1; i++) {
-    //         ans += min(left_max[i], right_max[i]) - height[i];
-    //     }
-    //     return ans;
-    // }
 
-    //editorial stack
-    int trap(vector<int>& height)
-    {
-        int ans = 0, current = 0;
-        stack<int> st; //indices
-        while (current < height.size()) {
-            while (!st.empty() && height[current] > height[st.top()]) {
-                int top = st.top();
-                st.pop();
-                if (st.empty())
-                    break;
-                int distance = current - st.top() - 1;
-                int bounded_height = min(height[current], height[st.top()]) - height[top];
-                ans += distance * bounded_height;
+    int firstUniqChar(string s) {
+        if (s.length() == 0)
+            return -1;
+        std::map<char,int> ind;
+        int min = s.length();
+        for (int i = 0; i < s.length(); ++i){
+            if (ind.find(s[i]) != ind.end()){
+                ind[s[i]] = -1;
             }
-            st.push(current++);
+            else{
+                ind[s[i]] = i;
+            }
         }
-        return ans;
+        for (auto i = ind.begin(); i != ind.end(); ++i){
+            if (i->second != -1){
+                if (i->second < min){
+                    min = i->second;
+                }
+            }
+        }
+        return (min == s.length())?(-1):min;
     }
 
 
 
 int main(){
-    std::vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    std::string s = "aa";
     
-    std::cout << trap(height) << "\n";
+    std::cout << firstUniqChar(s) << "\n";
 
 }
