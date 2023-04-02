0aaeddae265c0e3c1a1657bf640ccce8dfbf4d51
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-02 20:02:20 +0400

    42. Trapping Rain Water. Hard. NOT SOLVED

--- main.cpp
+++ main.cpp
@@ -13,58 +13,73 @@
 using namespace std;
 
 /*
-150. Evaluate Reverse Polish Notation
-You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
-
-Evaluate the expression. Return an integer that represents the value of the expression.
-
-Note that:
-
-The valid operators are '+', '-', '*', and '/'.
-Each operand may be an integer or another expression.
-The division between two integers always truncates toward zero.
-There will not be any division by zero.
-The input represents a valid arithmetic expression in a reverse polish notation.
-The answer and all the intermediate calculations can be represented in a 32-bit integer.
-
-1 <= tokens.length <= 104
-tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+42. Trapping Rain Water
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
 */
+    //editorial brute force
+    //     int trap(vector<int>& height) {
+    //     int ans = 0;
+    //     int size = height.size();
+    //     for (int i = 1; i < size - 1; i++) {
+    //         int left_max = 0, right_max = 0;
+    //         for (int j = i; j >= 0; j--) { //Search the left part for max bar size
+    //             left_max = max(left_max, height[j]);
+    //         }
+    //         for (int j = i; j < size; j++) { //Search the right part for max bar size
+    //             right_max = max(right_max, height[j]);
+    //         }
+    //         ans += min(left_max, right_max) - height[i];
+    //     }
+    //     return ans;
+    // }
 
-    int evalRPN(vector<string>& tokens) {
-        if (tokens.size() == 1)
-            return std::stoi(tokens[0]);
-        int result = 0, first = 0, second = 0;
-        std::stack<int> s;
-        for (size_t i = 0; i < tokens.size(); ++i){
-            if (tokens[i] == "+" ||  tokens[i] == "-" || tokens[i] == "/" || tokens[i] == "*"){
-                    second = s.top(); s.pop();
-                    first = s.top(); s.pop();
-                if (tokens[i] == "+"){
-                    s.push(first + second);
-                }
-                else if (tokens[i] == "-"){
-                    s.push(first - second);
-                }
-                else if (tokens[i] == "/" ){
-                    s.push(first / second);
-                }
-                else{// '*'
-                    s.push(first * second);
-                }
+    //editorial dynamic programming
+    // int trap(vector<int>& height)
+    // {
+    //     if(height.empty())
+    //         return 0;
+    //     int ans = 0;
+    //     int size = height.size();
+    //     vector<int> left_max(size), right_max(size);
+    //     left_max[0] = height[0];
+    //     for (int i = 1; i < size; i++) {
+    //         left_max[i] = max(height[i], left_max[i - 1]);
+    //     }
+    //     right_max[size - 1] = height[size - 1];
+    //     for (int i = size - 2; i >= 0; i--) {
+    //         right_max[i] = max(height[i], right_max[i + 1]);
+    //     }
+    //     for (int i = 1; i < size - 1; i++) {
+    //         ans += min(left_max[i], right_max[i]) - height[i];
+    //     }
+    //     return ans;
+    // }
+
+    //editorial stack
+    int trap(vector<int>& height)
+    {
+        int ans = 0, current = 0;
+        stack<int> st; //indices
+        while (current < height.size()) {
+            while (!st.empty() && height[current] > height[st.top()]) {
+                int top = st.top();
+                st.pop();
+                if (st.empty())
+                    break;
+                int distance = current - st.top() - 1;
+                int bounded_height = min(height[current], height[st.top()]) - height[top];
+                ans += distance * bounded_height;
             }
-            else{//a valid signed or unsigned integer
-                s.push(std::stoi(tokens[i]));
-            }
+            st.push(current++);
         }
-        return s.top();
+        return ans;
     }
 
 
+
 int main(){
-    std::vector<std::string> tokens = {"10","6","9","3","+","-11","*","/","*","17","+","5","+"};
-    // std::cout << std::stoi("-100") << "\n";
+    std::vector<int> height = {0,1,0,2,1,0,1,3,2,1,2,1};
     
-    std::cout << evalRPN(tokens) << "\n";
+    std::cout << trap(height) << "\n";
 
 }
