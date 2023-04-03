42de6fc171e52f2d610f5fb3dca3c21b76c48e03
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-03 11:42:14 +0400

    121. Best Time to Buy and Sell Stock. Easy

--- main.cpp
+++ main.cpp
@@ -13,58 +13,35 @@
 using namespace std;
 
 /*
-142. Linked List Cycle II
-Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+121. Best Time to Buy and Sell Stock
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
-Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
-Note that pos is not passed as a parameter.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
-Do not modify the linked list.
-The number of the nodes in the list is in the range [0, 104].
--105 <= Node.val <= 105
-pos is -1 or a valid index in the linked-list.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
 */
 
- // Definition for singly-linked list.
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
- 
-    ListNode *detectCycle(ListNode *head) {
-        if (!head)
-            return head;
-        std::unordered_set<ListNode*> s;
-        while (head){
-            auto it = s.find(head);
-            if (it == s.end())
-                s.emplace(head);
-            else
-                return head;
-            head = head->next;
-        }
-        return head;
+int maxProfit(vector<int>& prices) {
+    if (prices.size() < 2)
+        return 0;
+    int size = prices.size();
+    int result = 0;
+    std::vector<int> min(size);
+    std::vector<int> max(size);
+    min[0] = prices[0]; max[size - 1] = prices[size -1];
+    for (int l = 0, r = size - 2; l < size || r >= 0; ++l, --r){
+        min[l] = (prices[l] < min[l - 1])?(prices[l]):min[l - 1];
+        max[r] = (prices[r] > max[r + 1])?(prices[r]):max[r + 1];
     }
-
-
+    for (int i = 0; i < size; ++i){
+        result = (max[i] - min[i] > result)?(max[i] - min[i]):result;
+    }
+    return result;
+}
 
 int main(){
-    //head = [1,2,3,4,5]
-    //1,2,3,4,5,6
-    ListNode *head = new ListNode(1);
-    head->next = new ListNode(2);
-    ListNode * c = head->next;
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = c;
-
-    // head->next->next->next->next = new ListNode(5);
-    
-    ListNode* result = detectCycle(head);
-    std::cout<< " End\n";
-
+    std::vector<int> prices = {1};
+    std::cout << maxProfit(prices) << "\n";
 }
