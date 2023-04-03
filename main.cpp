d5aab1fcac040a60c6bdece06a124baff381afdb
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-03 07:21:41 +0400

    876. Middle of the Linked List. Easy

--- main.cpp
+++ main.cpp
@@ -13,11 +13,13 @@
 using namespace std;
 
 /*
-206. Reverse Linked List
-Given the head of a singly linked list, reverse the list, and return the reversed list.
+876. Middle of the Linked List
+Given the head of a singly linked list, return the middle node of the linked list.
 
-The number of nodes in the list is the range [0, 5000].
--5000 <= Node.val <= 5000
+If there are two middle nodes, return the second middle node.
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
 */
 
  // Definition for singly-linked list.
@@ -29,30 +31,30 @@ The number of nodes in the list is the range [0, 5000].
       ListNode(int x, ListNode *next) : val(x), next(next) {}
   };
  
-    ListNode* reverseList(ListNode* head) {
-        //if empty head, return itself
+    ListNode* middleNode(ListNode* head) {
         if (!head)
             return head;
-        ListNode *r = new ListNode (head->val);
-        head = head->next;
-        while (head != nullptr){
-            r = new ListNode(head->val, r);
+        std::vector<ListNode*> vec;
+        while (head)
+        {
+            vec.push_back(new ListNode(head->val, head->next));
             head = head->next;
         }
-        return r;
+        return vec[vec.size()/2];
     }
 
 
 
 int main(){
     //head = [1,2,3,4,5]
+    //1,2,3,4,5,6
     ListNode *head = new ListNode(1);
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
+    // head->next->next->next->next = new ListNode(5);
     
-    ListNode* result = reverseList(head);
+    ListNode* result = middleNode(head);
     std::cout <<" End\n";
 
 }
