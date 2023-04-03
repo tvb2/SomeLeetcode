0309ee5f91fa4fb4e891c975b6c12c1fd08da501
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-03 07:08:30 +0400

    206. Reverse Linked List. Easy

--- main.cpp
+++ main.cpp
@@ -13,33 +13,46 @@
 using namespace std;
 
 /*
-392. Is Subsequence
-Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+206. Reverse Linked List
+Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the 
-relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
-
-0 <= s.length <= 100
-0 <= t.length <= 104
-s and t consist only of lowercase English letters.
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000
 */
 
-    //searching t for symbols from s starting at the position of previousely found symbol
-    bool isSubsequence(string s, string t) {
-        size_t it = 0, j = 0; // indices of t to search for symbols from s
-        for (size_t i = 0; i < s.size(); ++i){//if there is no occurrence of s[i] in t from pos=it to the end of the string, return false
-            j = t.find_first_of(s[i], it); 
-            if (j == std::string::npos)
-                return false;
-            it = j+1;
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
+    ListNode* reverseList(ListNode* head) {
+        //if empty head, return itself
+        if (!head)
+            return head;
+        ListNode *r = new ListNode (head->val);
+        head = head->next;
+        while (head != nullptr){
+            r = new ListNode(head->val, r);
+            head = head->next;
         }
-        return true;
+        return r;
     }
 
 
 
 int main(){
-    std::string s = "axc", t = "ahbgdc";
-    std::cout<< boolalpha << isSubsequence(s, t) << "\n";
+    //head = [1,2,3,4,5]
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+    
+    ListNode* result = reverseList(head);
+    std::cout <<" End\n";
 
 }
