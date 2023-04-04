9969565d64bc19306fc3f64f126e9a638aa6a5e8
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-04 06:48:58 +0400

    589. N-ary Tree Preorder Traversal. Easy

--- main.cpp
+++ main.cpp
@@ -13,38 +13,74 @@
 using namespace std;
 
 /*
-409. Longest Palindrome
-Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+589. N-ary Tree Preorder Traversal
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
 
-Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
-1 <= s.length <= 2000
-s consists of lowercase and/or uppercase English letters only.
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
+The height of the n-ary tree is less than or equal to 1000.
 */
 
-int longestPalindrome(string s) {
-    if (s.length() == 0)
-        return 0;
-    if (s.length() == 1)
-        return 1;
-    int result = 0;
-    std::map<char, int> alpha;
-    for (size_t i = 0; i < s.length(); ++i){
-        ++alpha[s[i]];
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
     }
-    bool plusOne = false;
-    for (auto it = alpha.begin(); it != alpha.end(); ++it){
-        if (it->second > 1){
-            result += it->second/2*2;
-        }
-        if (it->second%2 == 1 && result%2 == 0)
-                ++result;
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
     }
-    return result;
+};
+
+//recursive solution going through all children and recursively pushing back each element to result vector
+void helper(Node* root, std::vector<int> &result){
+    result.push_back(root->val);
+    for (auto &it:root->children){
+        helper(it, result);
+    }
+}
+
+vector<int> preorder(Node* root) {
+    std::vector<int> result;
+    if (!root)
+        return result;
+    helper(root, result);
+    return result;    
 }
 
 int main(){
-    std::string s = "ccc";
-    // std::string s = "a";
-    std::cout << longestPalindrome(s) << "\n";
+    //[1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+    Node* root = new Node(1);
+    std::vector<Node*> vec;
+
+    Node* a = new Node(14);
+    vec.push_back(a);
+    Node* x = new Node(11, vec);
+    
+    vec.clear();
+    vec.push_back(x);
+    Node* y = new Node(7, vec);
+
+    vec.clear();
+    Node* z = new Node(6, vec);
+    vec.push_back(z);
+    vec.push_back(y);
+
+    Node* three = new Node(3, vec);
+
+    
+
+
+    std::vector<int> result = preorder(three);
+    for (auto i:result)
+        std::cout << i << "\n";
 }
