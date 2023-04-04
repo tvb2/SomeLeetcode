316a7ff6e26d75b3c3d847dbce1c85e8f0ff2109
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-04 08:51:04 +0400

    704. Binary Search. Easy

--- main.cpp
+++ main.cpp
@@ -13,67 +13,32 @@
 using namespace std;
 
 /*
-102. Binary Tree Level Order Traversal
-Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
-The number of nodes in the tree is in the range [0, 2000].
--1000 <= Node.val <= 1000
+704. Binary Search
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
 */
 
-//Definition for a binary tree node.
-  struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode() : val(0), left(nullptr), right(nullptr) {}
-      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-  };
- 
-/// @brief recursively traverse the tree, keeping track of at what level we are
-/// @param root 
-/// @param result 
-/// @param level keeps track of what level we are at. It is incremented/decremented according to going down/up the tree 
-void helper (TreeNode* root, std::vector<std::vector<int>> & result, int &level){
-    if (root){
-        std::vector<int> t{root->val};
-        if (result.size() < level){
-            result.emplace_back(t);
+int search(vector<int>& nums, int target) {
+    int l = 0, r = nums.size() - 1, ind = l;
+    ind = l + (r - l)/2;
+    while ( l >=0 && r < nums.size() && l <= r){
+        ind = l + (r - l)/2;
+        if (nums[ind] == target){
+            return ind;
         }
-        else{
-            result[level - 1].emplace_back(root->val);
+        if (nums[ind] > target){
+            r = ind -1;
         }
-        ++level;
-        helper(root->left,result, level);
-        ++level;
-        helper(root->right, result, level);
+        else {
+            l = ind +1;
+        }
     }
-    --level;
-
-}
-
-vector<vector<int>> levelOrder(TreeNode* root) {
-    vector<vector<int>> result;
-    int level = 1;
-    helper(root, result, level);
-    return result;    
+    return -1;
 }
 
 int main(){
-    //{[3,9,20,null,null,15,7]}
-    TreeNode* root = new TreeNode(3);
-    root->left = new TreeNode(9);
-    root->left->left = new TreeNode(8);
-    root->left->right = new TreeNode(6);
-
-    root->right = new TreeNode(20);
-    root->right->left = new TreeNode(15);
-    root->right->right = new TreeNode(7);
-
-    std::vector<std::vector<int>> result = levelOrder(root);
-    for (auto i:result){
-        for (auto j:i){
-            std::cout << j << " ";
-        }
-        std::cout <<"\n";
-    }
+    std::vector<int> nums = {-1,0,3,5,9,12};
+    int target = 2;
+    std::cout << search(nums, target);
 }
