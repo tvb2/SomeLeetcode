c6381e0b0817eaf507206306bd12c7b48467580a
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-04 20:05:48 +0400

    98. Validate Binary Search Tree. Medium

--- main.cpp
+++ main.cpp
@@ -13,39 +13,63 @@
 using namespace std;
 
 /*
-278. First Bad Version
-You are a product manager and currently leading a team to develop a new product. 
-Unfortunately, the latest version of your product fails the quality check. 
-Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+98. Validate Binary Search Tree. Medium
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
 
-Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+A valid BST is defined as follows:
 
-You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. 
-You should minimize the number of calls to the API.
-1 <= bad <= n <= 2^31 - 1
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+The number of nodes in the tree is in the range [1, 104].
+-2^31 <= Node.val <= 2^31 - 1
 */
-    
-    int bad = 2;
-    // The API isBadVersion is defined for you.
-    bool isBadVersion(int version){ return version >= bad;}
-
-    //using binary search to identify the first bad version
-    int firstBadVersion(int n) {
-        if (n == 1)
-            return n;
-        int l = 1, r = n, t = n;
-        while (l < r){
-            t = l + (r-l)/2;
-            if (!isBadVersion(t))
-                l = t+1;
-            else
-                r = t;
-        }
-        return (l==r)?l:t;
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+    //using recursive approach
+    bool helper (TreeNode* root, long long  min, long long  max){
+        //if we have reached the end of the tree, obviousely we are good
+        if (!root)
+            return true;
+        //if left element is not less than root - not Valid
+        if (root->left && (root->left->val >= root->val || root->left->val >= max || root->left->val <= min))
+            return false;
+        //if right element is not greater than root - not Valid
+        if (root->right && (root->right->val <= root->val || root->right->val >= max || root->right->val <= min))
+            return false;
+        //recursively check both left and right branches. Both must be valid.
+        return (helper(root->left, min, root->val) && helper(root->right, root->val, max));
     }
 
 
+    bool isValidBST(TreeNode* root) {
+        if (!root)
+            return true;
+        //long long is used to take into account the case when one of the values is min or max and it should be valid
+        long long min = static_cast<long long>(std::numeric_limits<int>::min()) - 1; 
+        long long max = static_cast<long long>(std::numeric_limits<int>::max()) + 1;
+        return (helper(root, min, max));
+    }
+
 int main(){
-    int n = 2;
-    std::cout << firstBadVersion(n);
+    // 3,1,5,0,2,4,6  -2147483648,null,2147483647
+    TreeNode* root = new TreeNode(-2147483648);
+    // root->left = new TreeNode(26);
+    // root->left->left = new TreeNode(19);
+    // root->left->left->right = new TreeNode(27);
+    
+    root->right = new TreeNode(2147483647);
+    // root->right->right= new TreeNode(56);
+    // root->right->right = new TreeNode(7);
+    
+    std::cout<< boolalpha << isValidBST(root) <<"\n";
 }
