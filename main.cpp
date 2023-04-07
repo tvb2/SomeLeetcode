00306afff1b338575a01518049e6c84aa37f6b5a
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-07 13:22:15 +0400

    62. Unique Paths. Medium

--- main.cpp
+++ main.cpp
@@ -14,27 +14,32 @@
 using namespace std;
 
 /*
-746. Min Cost Climbing Stairs
-You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+62. Unique Paths. Medium
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
+The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
-You can either start from the step with index 0, or the step with index 1.
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 
-Return the minimum cost to reach the top of the floor.
-
-2 <= cost.length <= 1000
-0 <= cost[i] <= 999
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+1 <= m, n <= 100
 */
 
-int minCostClimbingStairs(vector<int>& cost) {
-    size_t i = 2;
-    for (; i < cost.size(); ++i){
-        cost[i] += std::min(cost[i - 1], cost[i - 2]);
+int uniquePaths(int m, int n) {
+    std::vector<std::vector<int>> field(m);
+    //initialize the field with ones (basic case for row 1 and colum 1)
+    for (auto &i:field)
+        i.resize(n, 1);
+    //number of combinations for other cells is sum of [i-1][j] and [i][j-1]
+    for (size_t i = 1; i < field.size(); ++i){
+        for (size_t j = 1; j < field[i].size(); ++j){
+            field[i][j] = field[i - 1][j] + field[i][j - 1];
+        }
     }
-    return std::min(cost[i - 1], cost[i - 2]);
+ return field[m - 1][n - 1];   
 }
 
 int main(){
-    std::vector<int> cost =  {0,0,0,0};// {0,0,0,0}; // {1};// {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};// {10,15,20};
-    std::cout << minCostClimbingStairs(cost) << "\n";
+    int m = 5, n = 5;
+     std::cout << uniquePaths(m, n) << "\n";
 
 }
