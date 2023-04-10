d3575c87ab126227e59d3680fd84152cc1ca30bc
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-10 10:17:30 +0400

    299. Bulls and Cows.Medium

--- main.cpp
+++ main.cpp
@@ -14,33 +14,50 @@
 using namespace std;
 
 /*
-62. Unique Paths. Medium
-There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). 
-The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+299. Bulls and Cows
+You are playing the Bulls and Cows game with your friend.
 
-Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+You write down a secret number and ask your friend to guess what the number is. When your friend makes a guess, you provide a hint with the following info:
 
-The test cases are generated so that the answer will be less than or equal to 2 * 109.
-1 <= m, n <= 100
+The number of "bulls", which are digits in the guess that are in the correct position.
+The number of "cows", which are digits in the guess that are in your secret number but are located in the wrong position. Specifically, the non-bull digits in the guess that could be rearranged such that they become bulls.
+Given the secret number secret and your friend's guess guess, return the hint for your friend's guess.
+
+The hint should be formatted as "xAyB", where x is the number of bulls and y is the number of cows. Note that both secret and guess may contain duplicate digits.
+1 <= secret.length, guess.length <= 1000
+secret.length == guess.length
+secret and guess consist of digits only.
 */
 
-int uniquePaths(int m, int n) {
-    std::vector<std::vector<int>> field(m);
-    //initialize the field with ones (basic case for row 1 and colum 1)
-    for (auto &i:field)
-        i.resize(n, 1);
-    //number of combinations for other cells is sum of [i-1][j] and [i][j-1]
-    for (size_t i = 1; i < field.size(); ++i){
-        for (size_t j = 1; j < field[i].size(); ++j){
-            field[i][j] = field[i - 1][j] + field[i][j - 1];
+    /*using two freq maps s and g for secret and guess;
+    than compare each symbol fo secret with same position in guess
+    if they are the same, increment bulls
+    if they are not the same, increment freq of secret[i] in s and guess[i] in g
+    finally, cows is a total sum of min of s[i] and g[i]
+    */
+    string getHint(string secret, string guess) {
+        std::string result = "";
+        std::map<int, int> s{{0,0}, {1,0}, {2,0}, {3, 0}, {4,0}, {5,0}, {6,0}, {7,0}, {8,0}, {9,0}}, g = s;
+        int bulls{0}, cows{0};
+        bulls = 0; cows = 0;
+        for (size_t i = 0; i < secret.length(); ++i){
+            if (secret[i] == guess[i]){
+                ++bulls;
+            }
+            else{
+                ++s[secret[i]];
+                ++g[guess[i]];
+            }
         }
-        return (r - l);
+        for (size_t i = 0; i < s.size(); ++i){
+            cows += std::min(s[i], g[i]);
+        }
+        result = std::to_string(bulls) + 'A' + std::to_string(cows) + 'B';
+    return result;
     }
- return field[m - 1][n - 1];   
-}
 
 int main(){
-    int m = 5, n = 5;
-     std::cout << uniquePaths(m, n) << "\n";
+     std::string secret = "2222", guess = "0000";
+     std::cout << getHint(secret, guess) << "\n";
 
 }
