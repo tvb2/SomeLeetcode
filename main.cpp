3bd30e988d3597087945bc02cb9b52040fd10d5d
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-18 06:37:35 +0400

    328. Odd Even Linked List. Medium

--- main.cpp
+++ main.cpp
@@ -15,12 +15,17 @@
 using namespace std;
 
 /*
-234. Palindrome Linked List. Easy
-Given the head of a singly linked list, return true if it is a 
-palindrome
- or false otherwise.
- The number of nodes in the list is in the range [1, 105].
-0 <= Node.val <= 9
+328. Odd Even Linked List. Medium
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+The number of nodes in the linked list is in the range [0, 104].
+-106 <= Node.val <= 106
 */
 
  struct ListNode {
@@ -31,27 +36,41 @@ palindrome
      ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
  
-    bool isPalindrome(ListNode* head) {
-        std::deque<int> l;
-        while(head){
-            l.push_back(head->val);
-            head = head->next;    
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head)
+            return head;
+        if (!head->next)
+            return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* second = even;
+        while (even && odd && even->next && odd->next){
+                odd->next = odd->next->next;
+                odd = odd->next;
+            if (even->next->next){
+                even->next = even->next->next;
+                even = even->next;
+            }
+            else{
+                even->next = nullptr;
+                even = even->next;
+            }
         }
-        while (l.size() > 1){
-            if (l.back() != l.front())
-                return false;
-            l.pop_back(); l.pop_front();
-        }
-    return true;
+            if (even){
+                even->next = nullptr;
+                even = even->next;
+            }
+        odd->next = second;
+        return head;
     }
 
 
 int main(){
 ListNode* head = new ListNode(1);
-// = [1,2,3,4,5], n = 2
-// head->next = new ListNode(2);
-// head->next->next = new ListNode(2);
-// head->next->next->next = new ListNode(1);
-// head->next->next->next->next = new ListNode(5);
-std::cout << boolalpha << isPalindrome(head) <<  " end\n";
+head->next = new ListNode(2);
+head->next->next = new ListNode(3);
+head->next->next->next = new ListNode(4);
+head->next->next->next->next = new ListNode(5);
+ListNode* result = oddEvenList(head);
+std::cout <<  " end\n";
 }
