63b223662787b614c7f9ac8a5843093ccd0deaef
Bogdan Tverdoi <bogdan.tverdoy@gmail.com>
2023-04-24 19:45:22 +0400

    Build Treenode with string input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <map>
 #include <algorithm>
@@ -46,56 +47,94 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-    //return maximum number of levels for the node
-    int maxLevels(TreeNode* root, int levels){
-        ++levels;
-        int l = 0, r = 0;
-        if (root->left || root->right){
-            l = (root->left)?maxLevels(root->left, levels):1;
-            r = (root->right)?maxLevels(root->right, levels):1;
+    //Level order tree creation
+    void createLevels(TreeNode* &root, std::vector<std::vector<TreeNode*>> &nVect, int level){
+        if (level == 0){//first (root) node
+            root = nVect[level][0];
+            createLevels(root, nVect, ++level);
         }
-        else
-            return levels;
-        levels = std::max(l,r);
-        return levels;
+        if (level < nVect.size()){//proceed while level is below max
+            if (nVect[level].size() > 1) {
+                root->left = *nVect[level].begin();
+                nVect[level].erase(nVect[level].cbegin());//delete elements after being used
+            }
+            if (root->left) createLevels(root->left, nVect, (level+1));
+            
+            if (nVect[level].size() > 1){
+                root->right = *nVect[level].begin();
+                nVect[level].erase(nVect[level].cbegin());
+            } 
+            if (root->right) createLevels(root->right, nVect, (level+1));
+        }
+        return;
     }
 
-     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0, diaL = 0, diaR = 0;
-        if (!root)
-            return 0;
-        if (!root->left && !root->right)
-            return 1;
-        int l = 0, r = 0;
-        if (root->left || root->right){
-            l = (root->left)?maxLevels(root->left, l):0;
-            r = (root->right)?maxLevels(root->right,r):0;
-            diameter = (diameter < (l + r))?(l + r):diameter;
-            diaL = (root->left)?diameterOfBinaryTree(root->left):diameter;
-            diaR = (root->right)?diameterOfBinaryTree(root->right):diameter;
-            diameter = std::max({diameter, diaL, diaR});
-            // balanced = (root->left && balanced)?isBalanced(root->left):balanced;
-            // balanced = (root->right && balanced)?isBalanced(root->right):balanced;
+    /**
+     * convert string to a vector of Nodes for level-order creation
+     * Each element of vector represents a level in the tree
+    */
+    std::vector<std::vector<TreeNode*>> getNodesVector(std::string nodes){
+        std::vector<std::vector<TreeNode*>> nVect;
+        std::string temp;
+        std::stringstream stream(nodes);
+        int level = 0, count = 1;
+        std::vector<TreeNode*> lvl;
+        while (!stream.eof()){//read data from string stream delimited by commas
+            int k = count;//add number of elements defined at previous level
+            count = 0;//calculate possible number of elements in the next level (equals number of non-null elements)
+            while (k > 0 && !stream.eof()){
+                std::getline(stream, temp,',');
+                if (temp == "null"){
+                    lvl.emplace_back(nullptr);
+                }
+                else{
+                    int val = std::stoi(temp);
+                    lvl.emplace_back(new TreeNode(val, nullptr, nullptr));
+                    count += 2;//if we come across a non-null element, possible leafs on the next level is two
+                }
+                --k;
+            }
+            //add a nullptr in the end - this will be required during building of the tree 
+            //we will delete each element as we build the tree. And need to keep at least one element in order to keep the structure of the vector
+            lvl.emplace_back(nullptr);
+            nVect.emplace_back(lvl);
+            lvl.clear();
         }
-        return diameter;
+        return nVect;
     }
 
+
+    /**
+     * convert string representing a list of nodes to a binary tree represented by a root.
+    */
+    TreeNode* buildTree(std::string nodes){
+        if (nodes.length() == 0)
+            return new TreeNode;
+        std::vector<std::vector<TreeNode*>> nVect = getNodesVector(nodes);
+        
+        int i = 0, level = 0;
+        TreeNode* root;
+        createLevels(root, nVect, level);
+        return root;
+}
 
 int main(){
-    // root = [3,9,20,null,null,15,7]
-    TreeNode* root = new TreeNode(4);
-        root->left = new TreeNode(2);
-            root->left->left = new TreeNode(1);
-            root->left->right = new TreeNode(3);
-                root->left->right->left = new TreeNode(2);
-                    root->left->right->left->left = new TreeNode(1);
-                    root->left->right->left->right = new TreeNode(8);
-                root->left->right->right= new TreeNode(4);
+    std::string nodes = "10,5,-3,3,2,null,11,3,-2,null,1";
+    // std::string nodes = "5,4,8,11,null,13,4,7,2,null,null,5,1";
+    TreeNode* root = buildTree(nodes);
+    // std::vector<int> nodes = {5,4,8,11,nullptr,13,4,7,2,nullptr,nullptr,nullptr,1};
+    // TreeNode* root = new TreeNode(4);
+    //     root->left = new TreeNode(2);
+    //         root->left->left = new TreeNode(1);
+    //         root->left->right = new TreeNode(3);
+    //             root->left->right->left = new TreeNode(2);
+    //                 root->left->right->left->left = new TreeNode(1);
+    //                 root->left->right->left->right = new TreeNode(8);
+    //             root->left->right->right= new TreeNode(4);
             
         // root->right = new TreeNode(7);
         //     root->right->left = new TreeNode(6);
         //     root->right->right = new TreeNode(9);
     
-    std::cout << diameterOfBinaryTree(root);
     std::cout << " complete\n";
 }
